9e1af487d04a3f63f33ab2164769c065f637aecc
Payton Green <paytonagreen@gmail.com>
2021-04-20 00:13:43 -0500

    ssl updates

--- pages/_app.js
+++ pages/_app.js
@@ -62,9 +62,9 @@ function MyApp({ Component, pageProps }) {
       "Content-Type": "application/json",
     }
   } else if (process.env.NODE_ENV === 'production') {
-    api = 'http://api.asommforyou.com/';
+    api = 'https://api.asommforyou.com/';
     headers = {
-      "Access-Control-Allow-Origin": "http://www.asommforyou.com",
+      "Access-Control-Allow-Origin": "https://www.asommforyou.com",
       "Content-Type": "application/json",
     }
   }
