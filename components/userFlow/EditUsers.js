ff58388ec298cb4900884502f355fe0348443bdf
Payton Green <paytonagreen@gmail.com>
2021-05-06 00:29:26 -0500

    <EditUsers /> cleanup

--- components/userFlow/EditUsers.js
+++ components/userFlow/EditUsers.js
@@ -7,45 +7,9 @@ import { headers } from '../../hooks/swr-switch';
 import Selector from '../styles/Selector';
 import EditUser from './EditUser';
 
-export default function EditUsers({ currentUser }) {
+export default function EditUsers() {
   const { data, isLoading } = useUsers();
-
-  const [savingStarted, setSavingStarted] = useState();
-  const [successMessage, setSuccessMesssage] = useState();
-  const [errorMessage, setErrorMessage] = useState();
-
   const { values, handleChange } = useForm(callback);
-
-  console.log(isLoading);
-
-  function callback() {
-    if (!savingStarted) {
-      setSavingStarted(true);
-      const url = `api/users/${currentUser.id}`;
-      const options = {
-        body: JSON.stringify({
-          is_admin: values.admin,
-        }),
-        headers,
-        method: 'PUT',
-        credentials: 'include',
-      };
-      fetch(url, options)
-        .then(async (res) => {
-          const data = await res.json();
-          if (!res.ok) {
-            throw Error(data.message);
-          } else {
-            console.log(data);
-            setSuccessMesssage('You did it!');
-          }
-        })
-        .catch((err) => {
-          setErrorMessage(err.message);
-          console.log(err);
-        });
-    }
-  }
 
   if (isLoading) return <p>Loading...</p>;
   if (!data) return <p>Loading...</p>;
@@ -54,8 +18,6 @@ export default function EditUsers({ currentUser }) {
     <>
         <Selector>
       <h2>Choose User</h2>
-      {successMessage && <p>{successMessage}</p>}
-      {errorMessage && <p>{errorMessage}</p>}
       <select value={values.user} name='user' id='user' onChange={handleChange}>
         {users.map((user) => {
           return (
