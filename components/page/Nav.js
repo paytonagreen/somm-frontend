fb10e8c10ab9b11bd01bca2ada1a05e895459780
Payton Green <paytonagreen@gmail.com>
2021-05-05 20:22:47 -0500

    <Nav /> tests

--- components/page/Nav.js
+++ components/page/Nav.js
@@ -3,8 +3,7 @@ import Link from 'next/link';
 import SignOut from '../userFlow/SignOut';
 import NavStyles from '../styles/NavStyles';
 
-export default function Nav({setCurrentUser, currentUser}) {
-  
+export default function Nav({ setCurrentUser, currentUser }) {
   return (
     <NavStyles>
       <Link href='/'>
@@ -13,50 +12,49 @@ export default function Nav({setCurrentUser, currentUser}) {
         </a>
       </Link>
       <ul>
-
-      {!currentUser && (
-        <>
-          <Link href='/signUp'>
-            <li><a>
-              <p>Sign Up</p>
-            </a></li>
-          </Link>
-          <Link href='/signIn'>
-            <li><a>
-              <p>Sign In</p>
-            </a></li>
-          </Link>
-        </>
-      )}
-      {currentUser && currentUser.is_admin && (
-        <>
-          <Link href='/pairing'>
-            <li><a>
-              <p>Pair by Protein</p>
-            </a></li>
-          </Link>
-          <Link href='/saucePairing'>
-            <li><a>
-              <p>Pair by Sauce</p>
-            </a></li>
-          </Link>
-          <Link href='/addProtein'>
-            <li><a>
-              <p>Add Protein</p>
-            </a></li>
-          </Link>
-          <Link href='/addWine'>
-            <li><a>
-              <p>Add Wine</p>
-            </a></li>
-          </Link>
-          <Link href='/addSauce'>
-            <li><a>
-              <p>Add Sauce</p>
-            </a></li>
-          </Link>
-        </>
-      )}
+        {!currentUser && (
+          <>
+            <Link href='/signUp'>
+              <li>
+                <a name='Sign Up'>Sign Up</a>
+              </li>
+            </Link>
+            <Link href='/signIn'>
+              <li>
+                <a name='Sign In'>Sign In</a>
+              </li>
+            </Link>
+          </>
+        )}
+        {currentUser && currentUser.is_admin && (
+          <>
+            <Link href='/pairing'>
+              <li>
+                <a>Pair by Protein</a>
+              </li>
+            </Link>
+            <Link href='/saucePairing'>
+              <li>
+                <a>Pair by Sauce</a>
+              </li>
+            </Link>
+            <Link href='/addProtein'>
+              <li>
+                <a>Add Protein</a>
+              </li>
+            </Link>
+            <Link href='/addWine'>
+              <li>
+                <a>Add Wine</a>
+              </li>
+            </Link>
+            <Link href='/addSauce'>
+              <li>
+                <a>Add Sauce</a>
+              </li>
+            </Link>
+          </>
+        )}
       </ul>
       {currentUser && <SignOut setCurrentUser={setCurrentUser} />}
     </NavStyles>
