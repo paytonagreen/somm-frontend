54a2a4d9857e5f92dbe1ede0a8e15833e4f7fd47
Payton Green <paytonagreen@gmail.com>
2021-05-08 21:01:26 -0500

    render improvements

--- components/page/Page.js
+++ components/page/Page.js
@@ -1,10 +1,22 @@
+import { useCurrentUser } from 'hooks/swr-hooks';
+
 import Nav from 'components/page/Nav';
 import { Content } from '../styles/AppStyles';
+import LoadingNav from './LoadingNav';
 
-export default function Page({ children, currentUser }) {
+export default function Page({ children }) {
+  const { data } = useCurrentUser();
+
+  if (!data)
+    return (
+      <>
+        <LoadingNav />
+        <Content>{children}</Content>
+      </>
+    );
   return (
     <>
-      <Nav currentUser={currentUser} />
+      {data.user && <Nav />}
       <Content>{children}</Content>
     </>
   );
