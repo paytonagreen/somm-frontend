edcd0118b862bd9e8d7d068df564b51567e9b67e
Payton Green <paytonagreen@gmail.com>
2021-05-11 18:20:26 -0500

    <AllWines/ > test

--- __tests__/AllWines.test.js
+++ __tests__/AllWines.test.js
@@ -3,13 +3,14 @@ import { screen } from '@testing-library/react';
 import { render } from 'lib/test-utils';
 
 import AllWines from 'components/wines/AllWines';
+import userEvent from '@testing-library/user-event';
 
 describe('<AllWines />', () => {
   beforeEach(() => {
     render(<AllWines />);
   });
 
-  it('renders a loader', () => { 
+  it('renders a loader', () => {
     expect(screen.getByText(/Loading.../i)).toBeInTheDocument();
   });
 
@@ -26,7 +27,17 @@ describe('<AllWines />', () => {
   });
 
   it('renders pagination button', async () => {
-    await screen.findByText(/Cabernet Sauvignon/i)
-    expect(await screen.findByRole('button', {name: 'Next'})).toBeInTheDocument();
-  })
+    await screen.findByText(/Cabernet Sauvignon/i);
+    expect(
+      await screen.findByRole('button', { name: 'Next' })
+    ).toBeInTheDocument();
+  });
+
+  it('renders a new page on button click', async () => {
+    await screen.findByText(/Cabernet Sauvignon/i);
+    const nextButton = screen.getByRole('button', {name: 'Next'})
+    await userEvent.click(nextButton)
+    const previousButton = await screen.findByRole('button', {name: 'Previous'})
+    expect(previousButton).toBeInTheDocument();
+  });
 });
